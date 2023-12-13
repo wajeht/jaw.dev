d09317f84694a2bba6718849108e7b489c3ae452
wajeht <58354193+wajeht@users.noreply.github.com>
2023-12-13 17:37:34 -0600

    feat: Add Nuxt and nuxtjs to cSpell dictionary and update robots.txt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: './src',
   modules: ['@nuxtjs/robots'],
+  robots: {
+    rules: {
+      UserAgent: '*',
+      Disallow: '/',
+      Comment: 'get out of here!'
+    }
+  }
 })
