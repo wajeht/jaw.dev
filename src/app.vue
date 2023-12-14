69ea55b9802a920600bbd56deab86a3ce5999710
wajeht <58354193+wajeht@users.noreply.github.com>
2023-12-14 10:25:57 -0600

    feat: Add Tailwind CSS and overrides to app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import '@/assets/css/tailwind.css'
+import '@/assets/css/overrides.css'
+</script>
 <template>
   <NuxtLayout>
     <NuxtLoadingIndicator color="#ca1a70" :duration="2000"  :throttle="0"/>
