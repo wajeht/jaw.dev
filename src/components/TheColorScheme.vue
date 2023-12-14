0a7c34ea431d65861a9f146644ff6ed81fbb3d60
Jaw <58354193+wajeht@users.noreply.github.com>
2023-12-14 00:16:51 -0600

    feat: social links

--- src/components/TheColorScheme.vue
+++ src/components/TheColorScheme.vue
@@ -3,8 +3,15 @@ const colorMode = useColorMode()
 </script>
 
 <template>
-  <select class="text-[#454545] dark:bg-black dark:text-[#d9d9d9]  font-sans text-base" v-model="colorMode.preference">
-    <option value="light">light</option>
-    <option value="dark">dark</option>
-  </select>
+  <div class="flex gap-5">
+    <!-- external -->
+    <NuxtLink to="https://www.linkedin.com/in/kyawsny/" target="_blank">linkedin</NuxtLink>
+    <NuxtLink to="https://github.com/wajeht" target="_blank">github</NuxtLink>
+
+    <!-- theme -->
+    <select class="text-[#454545] dark:bg-black dark:text-[#d9d9d9] text-sm" v-model="colorMode.preference">
+      <option value="light">☀️</option>
+      <option value="dark">🌚</option>
+    </select>
+  </div>
 </template>
