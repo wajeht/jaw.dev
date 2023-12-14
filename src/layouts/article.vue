702d154bf7000bfb637e101ce5d932e8b74fe0cc
wajeht <58354193+wajeht@users.noreply.github.com>
2023-12-13 19:40:46 -0600

    style: Update article and default layouts

--- src/layouts/article.vue
+++ src/layouts/article.vue
@@ -1,10 +1,8 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <main>
-    <TheHeader />
-    <slot />
-  </main>
+  <div class="dark:bg-black h-screen dark:text-[#D9D9D9]">
+    <main class="max-w-4xl mx-auto">
+      <TheHeader />
+      <slot />
+    </main>
+  </div>
 </template>
