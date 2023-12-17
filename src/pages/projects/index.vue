1f93f0358fd205bee83fe80023effc49a1661220
wajeht <58354193+wajeht@users.noreply.github.com>
2023-12-16 18:11:04 -0600

    refactor: Improve project display and add external links

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -6,9 +6,22 @@ const query: QueryBuilderParams = { path: '/projects', where: [{ layout: 'articl
 <template>
   <main>
     <ContentList :query="query" v-slot="{ list }">
-      <div v-for="article in list" :key="article._path">
-        <NuxtLink :to="article._path"><h2>{{ article.title }}</h2></NuxtLink>
-        <p>{{ article.description }}</p>
+      <div v-for="project in list" :key="project._path">
+        <h2 class="flex gap-2 items-baseline">
+          <NuxtLink class="flex gap-1 items-baseline" :to="project.demo" :title="`${project.title}-demo`" target="_blank">
+            <span>
+              {{ project.title }}
+            </span>
+            <Icon name="↗" class="!text-base" />
+          </NuxtLink>
+
+          <NuxtLink
+            class="flex justify-center text-[#454545] dark:text-[#d9d9d9] visited:text-current dark:visited:text-current hover:text-black dark:hover:text-white"
+            :to="project.github" target="_blank" :title="`${project.title}-source-code`">
+            <IconCSS name="mdi:github" class="text-sm" />
+          </NuxtLink>
+        </h2>
+        <p>{{ project.description }}</p>
       </div>
     </ContentList>
   </main>
