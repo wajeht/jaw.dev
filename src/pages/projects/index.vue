a21bf0a8dbd5d5a24c5771ad976283ef905f9d4e
Jaw <58354193+wajeht@users.noreply.github.com>
2023-12-17 03:44:53 -0600

    chore(deps): update and upgrade

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -4,24 +4,22 @@ const query: QueryBuilderParams = { path: '/projects', limit: 5, sort: [{ date: 
 </script>
 
 <template>
-  <main>
-    <ContentList :query="query" v-slot="{ list }">
-      <div v-for="project in list" :key="project._path">
-        <h2 class="flex gap-2 items-center">
-          <NuxtLink class="flex items-center" style="text-decoration: none !important;" :to="project.demo"
-            :title="`${project.title}-demo`" target="_blank">
-            <span class="hover:underline"> {{ project.title }} </span>
-            <IconCSS name="mdi:arrow-top-right" class="text-base" />
-          </NuxtLink>
+  <ContentList :query="query" v-slot="{ list }">
+    <div v-for="project in list" :key="project._path">
+      <h2 class="flex gap-2 items-center">
+        <NuxtLink class="flex items-center" style="text-decoration: none !important;" :to="project.demo"
+          :title="`${project.title}-demo`" target="_blank">
+          <span class="hover:underline"> {{ project.title }} </span>
+          <IconCSS name="mdi:arrow-top-right" class="text-base" />
+        </NuxtLink>
 
-          <NuxtLink
-            class="flex justify-center text-[#454545] dark:text-[#d9d9d9] visited:text-current dark:visited:text-current hover:text-black dark:hover:text-white"
-            :to="project.github" target="_blank" :title="`${project.title}-source-code`">
-            <IconCSS name="mdi:github" class="text-lg" />
-          </NuxtLink>
-        </h2>
-        <p>{{ project.description }}</p>
-      </div>
-    </ContentList>
-  </main>
+        <NuxtLink
+          class="flex justify-center text-[#454545] dark:text-[#d9d9d9] visited:text-current dark:visited:text-current hover:text-black dark:hover:text-white"
+          :to="project.github" target="_blank" :title="`${project.title}-source-code`">
+          <IconCSS name="mdi:github" class="text-lg" />
+        </NuxtLink>
+      </h2>
+      <p>{{ project.description }}</p>
+    </div>
+  </ContentList>
 </template>
