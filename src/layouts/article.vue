dbbb9cd6c8fe35246829bbdb181c33ecaf24dbb2
wajeht <58354193+wajeht@users.noreply.github.com>
2023-12-16 23:27:16 -0600

    refactor: Simplify computed properties and variable names

--- src/layouts/article.vue
+++ src/layouts/article.vue
@@ -1,26 +1,23 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import type { NavItem } from '@nuxt/content/dist/runtime/types/index';
-import { computed, ref } from 'vue';
-import { useRoute } from 'vue-router';
-
-const { data } = await useAsyncData('navigation', () => fetchContentNavigation());
-const articles = ref<NavItem[]>([]);
-
-data.value?.filter((d) => {
-  if (d.title === 'Articles') {
-    articles.value = d.children as NavItem[];
-  }
-});
 
 const route = useRoute();
 
-const navigation = computed(() => {
-  const currentIndex = articles.value.findIndex((article) => article._path === route.path);
-  const prev = articles.value[currentIndex - 1] || null;
-  const next = articles.value[currentIndex + 1] || null;
-  return { prev, next };
+const { data } = await useAsyncData('navigation', () => fetchContentNavigation());
+
+const articlesData = computed(() => {
+  const articlesSection = data.value?.find((d) => d.title === 'Articles');
+  return articlesSection ? articlesSection.children as NavItem[] : [];
 });
 
+const navigationLinks = computed(() => {
+  const currentIndex = articlesData.value.findIndex((article) => article._path === route.path);
+  return {
+    prev: articlesData.value[currentIndex - 1] || null,
+    next: articlesData.value[currentIndex + 1] || null,
+  };
+});
 </script>
 
 <template>
@@ -30,22 +27,22 @@ const navigation = computed(() => {
     <slot />
 
     <div class="flex justify-between">
-      <p v-if="!navigation.prev && navigation.next"></p>
-      <p v-if="navigation.prev && !navigation.next"></p>
+      <p v-if="!navigationLinks.prev && navigationLinks.next"></p>
+      <p v-if="navigationLinks.prev && !navigationLinks.next"></p>
 
-      <p v-if="navigation.prev" class="order-first">
-        <NuxtLink :to="navigation.prev._path">
+      <template v-if="navigationLinks.prev">
+        <NuxtLink :to="navigationLinks.prev._path" class="order-first">
           <IconCSS name="mdi:arrow-left" class="me-[.5]" />
-          <span> {{ navigation.prev.title }} </span>
+          {{ navigationLinks.prev.title }}
         </NuxtLink>
-      </p>
+      </template>
 
-      <p v-if="navigation.next" class="order-last">
-        <NuxtLink :to="navigation.next._path">
-          <span> {{ navigation.next.title }} </span>
+      <template v-if="navigationLinks.next">
+        <NuxtLink :to="navigationLinks.next._path" class="order-last">
+          {{ navigationLinks.next.title }}
           <IconCSS name="mdi:arrow-right" class="ms-[.5]" />
         </NuxtLink>
-      </p>
+      </template>
     </div>
   </main>
 </template>
